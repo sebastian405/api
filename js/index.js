5957d14e716e241727d51c7262fb6ef731c1b98c
[deposada74] <[deposada74@misena.edu.co]>
2022-09-14 15:48:50 -0500

    Segundo guardado de archivos

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// let nombre = `Sebastian`;
+// const suma = 2+2;
+// const sumar = () => suma;
+// console.log(`Hola, Bienvenido:  ${nombre}`);
+// console.log(sumar());
+
+
---
+++ js/miApi.js
@@ -0,0 +1,1 @@
+fetch('datos.txt');
